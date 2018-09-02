f197c6f86dfbd84f5d34be003a827e335972ccd0
Jeremy Zacharia <jczacharia@gmail.com>
2018-09-02 15:41:11 -0400

    Added comments

--- Src/main.cpp
+++ Src/main.cpp
@@ -1,9 +1,9 @@
 //============================================================================
-// Name        : jLog.cpp
+// Name        : main.cpp
 // Author      : Jeremy C. Zacharia
 // Version     :
 // Copyright   : (C) Jeremy C. Zacharia
-// Description : Hello World in C++, Ansi-style
+// Description : jLog Example
 //============================================================================
 
 #include <jLog.h>
@@ -12,7 +12,8 @@ int main()
 {
 	jLOG << "This is a logging message." << std::endl;
 	jWAR << "This is a warning message." << std::endl;
-	jERR << "This is an error  message." << std::endl;
+	jERR << "This is an error message." << std::endl;
 	jDBG << "This is a debug message." << std::endl;
+
 	return 0;
 }
