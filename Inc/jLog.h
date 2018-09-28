7faeefdeadea3e620264c2deb2704381a408451c
Jeremy Zacharia <jczacharia@gmail.com>
2018-09-27 20:23:30 -0400

    static instance

--- Inc/jLog.h
+++ Inc/jLog.h
@@ -11,47 +11,51 @@
 
 #include <iostream>
 #include <fstream>
-#include <memory>
 #include <chrono>
 #include <cstring>
 #include <ctime>
 #include <thread>
 #include <functional>
 #include <mutex>
-#include <cstdlib>
-#include <atomic>
-#include <exception>
 
+/* Singleton Templated Class */
+template<class T>
+class Singleton
+{
+protected:
+	/* Prevent instancing. */
+	Singleton() noexcept = default;					// Constructor
+	virtual ~Singleton() = default;					// Destructor
+	Singleton(Singleton const&) = delete;            	// Copy construct
+	Singleton(Singleton&&) = delete;                  	// Move construct
+	Singleton& operator=(Singleton const&) = delete;  	// Copy assign
+	Singleton& operator=(Singleton &&) = delete;      	// Move assign
+
+public:
+	static T& get()
+	{
+		static T instance;
+		return instance;
+	}
+};
+
+/* Available Logging Levels */
 enum class Level
 {
 	Log, War, Err, Dbg
 };
 
-template<class T>
-class Singleton
-{
-protected:
-	/* Here will be the instance stored. */
-	static std::atomic<T*> instance;
-	static std::mutex mutex;
-
-	/* Prevent instancing. */
-	Singleton() noexcept = default;
-	Singleton(const Singleton&) = delete;
-	Singleton& operator=(const Singleton&) = delete;
-	virtual ~Singleton() = default;
-
-public:
-	/* Static access method. */
-	static T* get() noexcept(std::is_nothrow_constructible<T>::value);
-
-};
-
+/* jLog Singleton */
 class jLog: private Singleton<jLog>
 {
 	friend Singleton<jLog> ;
-	jLog(const jLog&) = delete;
-	jLog& operator=(const jLog&) = delete;
+
+	/* Prevent instancing. */
+	~jLog() = default;							// Destructor
+	jLog(jLog const&) = delete;            	// Copy construct
+	jLog(jLog&&) = delete;                  	// Move construct
+	jLog& operator=(jLog const&) = delete;  	// Copy assign
+	jLog& operator=(jLog &&) = delete;      	// Move assign
 
 	jLog() :
 					_level(Level::Log),
@@ -68,7 +72,7 @@ public:
 		findAndReplace(str, "\n", "\n\t\t\t\t     ");
 
 		if(_new_log_entry) {
-			// set flag false so time and log isn't displayed
+			// set flag so log isn't duplicated
 			// 		for every << operation
 			_new_log_entry = false;
 
@@ -80,11 +84,8 @@ public:
 			_file_output << jLog::getNowTime()
 					<< jLog::logLevelToStringNoColor(_level) << str;
 		} else {
-			// Write to console
-			*_console_output << str;
-
-			// write to file
-			_file_output << str;
+			*_console_output << str; 	// Write to console
+			_file_output << str; 		// Write to file
 		}
 		return *this;
 	}
@@ -110,40 +111,37 @@ public:
 	static jLog& log(Level l = Level::Log)
 	{
 		_mutex.lock();
-		jLog& jlog = *jLog::get();
+		jLog& jlog = jLog::get();
 		jlog._level = l;
 		return jlog;
 	}
 
-	static void init(const char* const p = "", std::ostream& os = std::cout)
+	static void init(const char* const p, std::ostream& os = std::cout)
 	{
 		std::lock_guard<std::mutex> guard(_mutex);
-		jLog& jl = *jLog::get();
+		jLog& jl = jLog::get();
 		std::string file_path;
 
-		file_path.append("logs/");
+		file_path.append(p);
 
-		if(strcmp(p, "") == 0) {
-			// Get Now Time
-			auto now_time = std::chrono::system_clock::now();
-			std::time_t t_time = std::chrono::system_clock::to_time_t(now_time);
-			tm local_time = *localtime(&t_time);
+		// Get Now Time
+		auto now_time = std::chrono::system_clock::now();
+		std::time_t t_time = std::chrono::system_clock::to_time_t(now_time);
+		tm local_time = *localtime(&t_time);
 
-			file_path.append(std::to_string(local_time.tm_year + 1900));
-			file_path.append("-");
-			file_path.append(std::to_string(local_time.tm_mon));
-			file_path.append("-");
-			file_path.append(std::to_string(local_time.tm_mday));
-			file_path.append("_");
-			file_path.append(std::to_string(local_time.tm_hour));
-			file_path.append("-");
-			file_path.append(std::to_string(local_time.tm_min));
-			file_path.append("-");
-			file_path.append(std::to_string(local_time.tm_sec));
-			file_path.append(".txt");
-		} else {
-			file_path = p;
-		}
+		file_path.append(std::to_string(local_time.tm_year + 1900));
+		file_path.append("-");
+		file_path.append(std::to_string(local_time.tm_mon));
+		file_path.append("-");
+		file_path.append(std::to_string(local_time.tm_mday));
+		file_path.append("_");
+		file_path.append(std::to_string(local_time.tm_hour));
+		file_path.append("-");
+		file_path.append(std::to_string(local_time.tm_min));
+		file_path.append("-");
+		file_path.append(std::to_string(local_time.tm_sec));
+		file_path.append(".txt");
+		std::cout << file_path << std::endl;
 
 		jl._console_output = &os;
 		jl._file_output = std::ofstream(p, std::ios::out | std::ios::app);
@@ -191,7 +189,7 @@ private:
 
 	const char* const logLevelToStringColor(Level jll)
 	{
-		switch(_level) {
+		switch(jll) {
 		case Level::Log:
 			return " [   LOG   ] ";
 		case Level::War:
@@ -221,28 +219,5 @@ private:
 };
 
 std::mutex jLog::_mutex;
-
-template<typename T>
-std::mutex Singleton<T>::mutex;
-
-template<typename T>
-std::atomic<T*> Singleton<T>::instance;
-
-/* Static access method. */
-template<typename T>
-T* Singleton<T>::get()
-noexcept(std::is_nothrow_constructible<T>::value)
-{
-	T* sin = instance.load();
-	if(!sin) {
-		std::lock_guard<std::mutex> myLock(mutex);
-		sin = instance.load();
-		if(!sin) {
-			sin = new T;
-			instance.store(sin);
-		}
-	}
-	return sin;
-}
 
 #endif /* JLOG_H_ */
--- Src/gtestx_test.cpp
+++ Src/gtestx_test.cpp
@@ -12,71 +12,75 @@
 
 TEST(JLOG_TEST, init)
 {
-	jLog::init("", std::cout);
-}
-
-TEST(JLOG_TEST, noInitLog)
-{
-
+	jLog::init("logs/", std::cout);
 	jLog::log(Level::Log) << "This is a log message" << std::endl;
 	jLog::log(Level::War) << "This is a warning message." << std::endl;
 	jLog::log(Level::Err) << "This is an error message." << std::endl;
 	jLog::log(Level::Dbg) << "This is a debug message." << std::endl;
 }
 
-TEST(JLOG_TEST, initLog)
-{
-	jLog::init("", std::cout);
+/*TEST(JLOG_TEST, noInitLog)
+ {
 
-	jLog::log(Level::Log) << "This is a log message" << std::endl;
-	jLog::log(Level::War) << "This is a warning message." << std::endl;
-	jLog::log(Level::Err) << "This is an error message." << std::endl;
-	jLog::log(Level::Dbg) << "This is a debug message." << std::endl;
+ jLog::log(Level::Log) << "This is a log message" << std::endl;
+ jLog::log(Level::War) << "This is a warning message." << std::endl;
+ jLog::log(Level::Err) << "This is an error message." << std::endl;
+ jLog::log(Level::Dbg) << "This is a debug message." << std::endl;
+ }
 
-}
+ TEST(JLOG_TEST, initLog)
+ {
+ jLog::init("", std::cout);
 
-TEST(JLOG_TEST, noInitLogAsync)
-{
-	std::function<void()> f1 =
-			[]() {
-				jLog::log(Level::Log) << "This is a log message" << std::endl;
-				jLog::log(Level::War) << "This is a warning message." << std::endl;
-				jLog::log(Level::Err) << "This is an error message." << std::endl;
-				jLog::log(Level::Dbg) << "This is a debug message." << std::endl;
-			};
+ jLog::log(Level::Log) << "This is a log message" << std::endl;
+ jLog::log(Level::War) << "This is a warning message." << std::endl;
+ jLog::log(Level::Err) << "This is an error message." << std::endl;
+ jLog::log(Level::Dbg) << "This is a debug message." << std::endl;
 
-	std::function<void()> f2 =
-			[]() {
-				jLog::log(Level::Log) << "This is another log message" << std::endl;
-				jLog::log(Level::War) << "This is another warning message." << std::endl;
-				jLog::log(Level::Err) << "This is another error message." << std::endl;
-				jLog::log(Level::Dbg) << "This is another debug message." << std::endl;
-			};
+ }
 
-	std::async(f1);
-	std::async(f2);
-}
+ TEST(JLOG_TEST, noInitLogAsync)
+ {
+ std::function<void()> f1 =
+ []() {
+ jLog::log(Level::Log) << "This is a log message" << std::endl;
+ jLog::log(Level::War) << "This is a warning message." << std::endl;
+ jLog::log(Level::Err) << "This is an error message." << std::endl;
+ jLog::log(Level::Dbg) << "This is a debug message." << std::endl;
+ };
 
-TEST(JLOG_TEST, initLogAsync)
-{
-	jLog::init("", std::cout);
+ std::function<void()> f2 =
+ []() {
+ jLog::log(Level::Log) << "This is another log message" << std::endl;
+ jLog::log(Level::War) << "This is another warning message." << std::endl;
+ jLog::log(Level::Err) << "This is another error message." << std::endl;
+ jLog::log(Level::Dbg) << "This is another debug message." << std::endl;
+ };
 
-	std::function<void()> f1 =
-			[]() {
-				jLog::log(Level::Log) << "This is a log message" << std::endl;
-				jLog::log(Level::War) << "This is a warning message." << std::endl;
-				jLog::log(Level::Err) << "This is an error message." << std::endl;
-				jLog::log(Level::Dbg) << "This is a debug message." << std::endl;
-			};
+ std::async(f1);
+ std::async(f2);
+ }
 
-	std::function<void()> f2 =
-			[]() {
-				jLog::log(Level::Log) << "This is another log message" << std::endl;
-				jLog::log(Level::War) << "This is another warning message." << std::endl;
-				jLog::log(Level::Err) << "This is another error message." << std::endl;
-				jLog::log(Level::Dbg) << "This is another debug message." << std::endl;
-			};
+ TEST(JLOG_TEST, initLogAsync)
+ {
+ jLog::init("", std::cout);
 
-	std::async(f1);
-	std::async(f2);
-}
+ std::function<void()> f1 =
+ []() {
+ jLog::log(Level::Log) << "This is a log message" << std::endl;
+ jLog::log(Level::War) << "This is a warning message." << std::endl;
+ jLog::log(Level::Err) << "This is an error message." << std::endl;
+ jLog::log(Level::Dbg) << "This is a debug message." << std::endl;
+ };
+
+ std::function<void()> f2 =
+ []() {
+ jLog::log(Level::Log) << "This is another log message" << std::endl;
+ jLog::log(Level::War) << "This is another warning message." << std::endl;
+ jLog::log(Level::Err) << "This is another error message." << std::endl;
+ jLog::log(Level::Dbg) << "This is another debug message." << std::endl;
+ };
+
+ std::async(f1);
+ std::async(f2);
+ }*/
